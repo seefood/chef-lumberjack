6817fc148b22808c156d6fc20aaaf02fd085f961
Chris Lundquist <chris.lundquist@bluebox.net>
2013-01-25 16:49:58 -0800

    [metadata] add name

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license           "Apache 2.0"
 description       "Installs and configures Lumberjack."
 version           "0.1.1"
 recipe            "lumberjack", "Installs and configures Lumberjack"
+name              "lumberjack"
 
 %w{ logrotate logstash }.each do |d|
   depends d
